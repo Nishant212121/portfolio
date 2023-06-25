7a4d6194fd1c8907174da33639ba822bc7b07724
Nishant212121 <savitarjakane@gmail.com>
2023-06-25 12:04:11 +0530

    About Me add

---
+++ src/components/MainContent/SkillsSeciton.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function SkillsSection(){
+    return (
+        <div id="skills" cl>
+            <h1 className="skillsHeading text-3xl">My Skills</h1>
+            <div className="container mx-auto flex px-4 items-center justify-center flex-row lg:px-5 lg:py-10">
+            <p className="skillsPara text-xs lg:text-xl py-5" >
+            With intermediate proficiency in the MERN stack, I possess a solid foundation in MongoDB, Express.js, React, and Node.js. I have gained 
+            experience in developing full-stack web applications and I'm eager to further enhance my skills in this versatile technology stack.
+            </p>
+            <div className="row"></div>
+
+            </div>
+        </div>
+    )
+}
+
+export default SkillsSection;
